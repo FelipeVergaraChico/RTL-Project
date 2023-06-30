d5e1a176eb56f7209fd29e3f03658ddf80eee4fc
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-06-30 19:32:45 -0300

    requisito 2 e 3

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,10 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { About } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+test('A pagina contém as informações sobre a Pokédex', () => {
+  renderWithRouter(<About />);
+  expect(screen.getByText('About Pokédex')).toBeInTheDocument();
+  const imagem = screen.getByAltText('Pokédex') as HTMLImageElement;
+  expect(imagem.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Caso não tenha sido selecionado nenhum pokemon ao navegar para a pagina de favoritos ela esteja vazia', async () => {
+  renderWithRouter(<App />);
+
+  expect(screen.getByRole('heading', { name: /encountered pokémon/i })).toBeInTheDocument();
+  const link = screen.getByRole('link', { name: /Favorite Pokémon/i });
+  await userEvent.click(link);
+  expect(screen.getByRole('heading', { name: /Favorite Pokémon/i })).toBeInTheDocument();
+  expect(screen.getByText(/no favorite pokémon found/i)).toBeInTheDocument();
+});
+
+test('Caso tenha selecionado algum pokemon ao navegar para pagina de fav ela tenha o pokemon selecionado', async () => {
+  renderWithRouter(<App />);
+  expect(screen.getByRole('heading', { name: /encountered pokémon/i })).toBeInTheDocument();
+  const details = screen.getByRole('link', { name: /more details/i });
+  await userEvent.click(details);
+  expect(screen.getByRole('heading', { name: /Pikachu Details/i })).toBeInTheDocument();
+  const btn = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+  await userEvent.click(btn);
+  const favorite = screen.getByRole('link', { name: /Favorite Pokémon/i });
+  await userEvent.click(favorite);
+  expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+});
